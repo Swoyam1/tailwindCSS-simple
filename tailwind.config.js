734bd6f2121f4cf848f4035c2f816271ad1ae475
swoyam1 <swoyam1.pant@gmail.com>
2023-10-04 02:36:27 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      spacing: {
+        big: "30rem",
+      },
+    },
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
